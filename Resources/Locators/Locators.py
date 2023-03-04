f926d046b426551bf5f13f09744fa832e7c6c9c3
Yusuf Renkoglu <yusuf@renkoglu.com>
2023-03-04 23:41:44 +0200

    4.3.2023 / Yusuf

--- Resources/Locators/Locators.py
+++ Resources/Locators/Locators.py
@@ -1,12 +1,10 @@
-# HomePage Locators
+# Almamedia Locators
 cookiesButton="css=button#almacmp-modalConfirmBtn"
 palveluLink="(//a[@class='dropdown-toggle'])[3]"
 tuotteetPalvelutLink="Tuotteet ja palvelut"
-
-# Kaikki PalveluPage Locators
 kotikokkiLink="xpath=(//div[@class='alma-consumer palvelut-kuluttajille business-area-result col-md-3']/a)[4]"
 
-# Kotikokki HomePage Locators
+# Kotikokki Locators
 cookiesButtonKK="css=button#almacmp-modalConfirmBtn"
 searchBoxInput="xpath=(//input[@name='freeText'])[1]"
 haeButton="css=a#recipe-search-free-text-submit"
